23c9b15e0d80da73ff8ce3388e09e64e72c101e9
byohannes <42332389+byohannes@users.noreply.github.com>
2020-06-03 22:45:51 +0100

    footer added to the page and media query for mobile updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Heading from './components/Heading';
-import MainJobsBoard from './components/MainJobsBoard';
+import MainJobsBoard from './components/MainJobsBoard'; 
 
 const App = () => {
   return (
     <div>
       <Heading />
       <MainJobsBoard />
+      <div className="footer">
+        Challenge by{' '}
+        <a href="https://www.frontendmentor.io/challenges/job-listings-with-filtering-ivstIPCt">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/byohannes">Berhane Yohannes</a>.
+      </div>
     </div>
   );
 };
